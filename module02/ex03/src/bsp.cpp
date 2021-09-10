35572f0d2ac11164142a6149f35cb273ca596d57
mikastiv <m.leblanc_3@hotmail.com>
2021-09-10 02:16:32 -0400

    variable rename

--- module02/ex03/src/bsp.cpp
+++ module02/ex03/src/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 22:43:55 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/09/10 00:47:27 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/09/10 02:13:52 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ Fixed formula(const Point a, const Point b, const Point c)
 
 bool bsp(const Point a, const Point b, const Point c, const Point point)
 {
-    Fixed d = formula(a, b, c);
-    Fixed p1 = formula(point, b, c) / d;
-    Fixed p2 = formula(point, c, a) / d;
+    Fixed denom = formula(a, b, c);
+    Fixed p1 = formula(point, b, c) / denom;
+    Fixed p2 = formula(point, c, a) / denom;
     Fixed p3 = Fixed(1) - p1 - p2;
 
     return Fixed(0) < p1 && p1 < Fixed(1) && Fixed(0) < p2 && p2 < Fixed(1) &&
