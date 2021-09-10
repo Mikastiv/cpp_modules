8767e8f3ed250385755bec48d25e6f6848b25a18
mikastiv <m.leblanc_3@hotmail.com>
2021-09-09 21:54:59 -0400

    tests

--- module02/ex02/src/main.cpp
+++ module02/ex02/src/main.cpp
@@ -6,9 +6,11 @@
 /*   By: mleblanc <mleblanc@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 09:49:09 by mleblanc          #+#    #+#             */
-/*   Updated: 2021/09/09 20:43:26 by mleblanc         ###   ########.fr       */
+/*   Updated: 2021/09/09 21:54:25 by mleblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iomanip>
 
 #include "Fixed.hpp"
 
@@ -17,21 +19,57 @@ int main(void)
     Fixed       a;
     const Fixed b(Fixed(5.05f) * Fixed(2));
     Fixed       c(9.87f);
+    Fixed       d(c);
 
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
+    std::cout << "a   = " << a << '\n';
+    std::cout << "++a = " << ++a << '\n';
+    std::cout << "a   = " << a << '\n';
+    std::cout << "a++ = " << a++ << '\n';
+    std::cout << "a   = " << a << '\n';
+    std::cout << "--a = " << --a << '\n';
+    std::cout << "a   = " << a << '\n';
+    std::cout << "a-- = " << a-- << '\n';
+    std::cout << "a   = " << a << "\n\n";
 
-    std::cout << b << std::endl;
-    std::cout << Fixed::max(a, b) << std::endl;
+    std::cout << "b = " << b << '\n';
+    std::cout << "a = " << a << '\n';
+    std::cout << "max(a, b) = " << Fixed::max(a, b) << "\n\n";
 
-    std::cout << c << std::endl;
-    std::cout << Fixed::min(c, b) << std::endl;
+    Fixed t = Fixed::max(c, a);
+    t.setRawBits(0xFF0FA);
+    t = Fixed::min(a, d);
+    t.setRawBits(0xFF0FA);
 
-    std::cout << 9.87f / 4.5f << std::endl;
-    std::cout << c / Fixed(4.5f) << std::endl;
+    std::cout << "b = " << b << '\n';
+    std::cout << "c = " << c << '\n';
+    std::cout << "min(b, c) = " << Fixed::min(b, c) << "\n\n";
+
+    std::cout << "9.87f + 4.5f = " << 9.87f + 4.5f << '\n';
+    std::cout << "c + 4.5 = " << c + Fixed(4.5f) << "\n\n";
+
+    std::cout << "9.87f - 4.5f = " << 9.87f - 4.5f << '\n';
+    std::cout << "c - 4.5 = " << c - Fixed(4.5f) << "\n\n";
+
+    std::cout << "9.87f * 4.5f = " << 9.87f * 4.5f << '\n';
+    std::cout << "c * 4.5 = " << c * Fixed(4.5f) << "\n\n";
+
+    std::cout << "9.87f / 4.5f = " << 9.87f / 4.5f << '\n';
+    std::cout << "c / 4.5 = " << c / Fixed(4.5f) << "\n\n";
+
+    std::cout << std::setw(7) << a << std::setw(4) << " > " << std::setw(7) << b
+              << " = " << std::boolalpha << (a > b) << '\n';
+    std::cout << std::setw(7) << c << std::setw(4) << " >= " << std::setw(7)
+              << d << " = " << std::boolalpha << (c >= d) << '\n';
+    std::cout << std::setw(7) << a << std::setw(4) << " < " << std::setw(7) << b
+              << " = " << std::boolalpha << (a < b) << '\n';
+    std::cout << std::setw(7) << c << std::setw(4) << " <= " << std::setw(7)
+              << d << " = " << std::boolalpha << (c <= d) << '\n';
+    std::cout << std::setw(7) << c << std::setw(4) << " <= " << std::setw(7)
+              << b << " = " << std::boolalpha << (c <= b) << '\n';
+    std::cout << std::setw(7) << a << std::setw(4) << " == " << std::setw(7)
+              << b << " = " << std::boolalpha << (a == b) << '\n';
+    std::cout << std::setw(7) << c << std::setw(4) << " == " << std::setw(7)
+              << d << " = " << std::boolalpha << (c == d) << '\n';
 
     return 0;
 }
